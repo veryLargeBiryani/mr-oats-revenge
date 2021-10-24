c8ccb917b3d27e55b8a5ad1027b0a574c2c4d402
veryLargeBiryani <alex.hyams.98@gmail.com>
2021-10-24 17:51:06 -0400

    last commit left out updated bot_functions.js, which now only has cook, menu, and playlistLoader functions

--- bot_functions.js
+++ bot_functions.js
@@ -1,95 +1,10 @@
 // dependencies
 const ytdl = require('ytdl-core');
-const ytsr = require('ytsr');
-const { getData, getPreview, getTracks } = require('spotify-url-info');
+const {getTracks} = require('spotify-url-info');
 const ytpl = require('ytpl');
-const {prefix} = require('./config.json');
+const {getSpotifySong} = require('./getSong.js');
 
 // functions
-async function execute(message, serverQueue, queue){
-    const args = message.content.split(" ");
-    let searchTitle = "";
-    for (i = 1; i < args.length; i++){
-        searchTitle = `${searchTitle} ${args[i]}`;
-    }
-    console.log(`user searched for "${searchTitle}"`);
-
-    const voiceChannel = message.member.voice.channel;
-    if (!voiceChannel)
-        return message.channel.send(
-            "please enter the kitchen before cooking your meal."
-        );
-    const permissions = voiceChannel.permissionsFor(message.client.user);
-    if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
-        return message.channel.send(
-            "i do not have the necessary utensils to cook you a meal right now."
-        );
-    }
-
-    // prepare logic before adding song; decide if search should be performed
-    let song;
-    if (args.length < 2){
-        return message.channel.send("nothing was ordered.");
-    } else if (args[1].includes("http") > 0){ //if command had a direct link
-        if (args[1].includes("open.spotify.com/track/")){
-            song = await spotifyTrack(args[1]);
-            console.log(`Spotify track was found on youtube: ${song.title}`);
-        } else {
-            const songInfo = await ytdl.getInfo(args[1]);
-            song = {
-                title: songInfo.videoDetails.title,
-                url: songInfo.videoDetails.video_url
-            }
-        }
-    } else { //if command had a search
-        const filters1 = await ytsr.getFilters(searchTitle);
-        const filter1 = filters1.get('Type').get('Video');
-        if (filter1.url == null){
-            return message.channel.send("couldn't find anything to plate.");
-        } else {
-            const searchResults = await ytsr(filter1.url, {pages : 1});
-            song = {
-                title: searchResults.items[0].title,
-                url: searchResults.items[0].url
-            }
-        }
-    }
-
-    if (!serverQueue){
-        // creating contract for queue
-        const queueContract = {
-            textChannel: message.channel,
-            voiceChannel: voiceChannel,
-            connection: null,
-            songs: [],
-            volume: 5,
-            playing: true,
-        };
-    
-        // setting queue using contract
-        queue.set(message.guild.id, queueContract);
-        // push song to songs array
-        queueContract.songs.push(song);
-        
-        try {
-            // try to join voice and save connection into object
-            var connection = await voiceChannel.join();
-            queueContract.connection = connection;
-            // call play function and start song
-            cook(message.guild, queueContract.songs[0], queue);
-        } catch (err) {
-            // print error if bot fails join
-            console.log(err);
-            queue.delete(message.guild.id);
-            return message.channel.send("no cooks in the kitchen.");
-        }
-    } else {
-        serverQueue.songs.push(song);
-        console.log(serverQueue.songs);
-        return message.channel.send(`${song.title} has been added to your plate`);
-    }
-}
-
 async function cook(guild, song, queue){
     const serverQueue = queue.get(guild.id);
     if (!song) {
@@ -108,35 +23,12 @@ async function cook(guild, song, queue){
         })
         .on("error", e => {
             console.error(e);
-            //serverQueue.songs.shift();
             cook(guild, serverQueue.songs[0], queue); //restart song on socket error
         });
     dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
     serverQueue.textChannel.send(`breakfast is served: ${song.title}`);
     console.log('now playing', song);
     return;
-}
-
-function lunch(message, serverQueue){
-    console.log('user requested to skip');
-    if (!message.member.voice.channel)
-        return message.channel.send(
-            "only chefs can change the ingredients."
-        );
-    if (!serverQueue)
-            return message.channel.send("no other meals to prepare.");
-    serverQueue.connection.dispatcher.end();
-}
-
-function cancel(message, serverQueue){
-    if (!message.member.voice.channel)
-        return message.channel.send(
-            "only chefs can change the ingredients."
-        );
-    if (!serverQueue)
-            return message.channel.send("no dishes to throw out.");
-    serverQueue.songs = [];
-    serverQueue.connection.dispatcher.end();
 }
 
 function menu(message, serverQueue){
@@ -159,29 +51,13 @@ function menu(message, serverQueue){
     }
 }
 
-async function spotifyTrack(url){
-    let spotTrack = await getData(url); //grab metadata w/ url
-    let spotTrackInfo = `${spotTrack.name} ${spotTrack.artists[0].name}`;
-    const filtersSpot = await ytsr.getFilters(spotTrackInfo); //search for youtube url
-    const filterSpot = filtersSpot.get('Type').get('Video');
-    if (filterSpot.url == null){
-        return message.channel.send("couldn't find anything to plate."); //will probably need some error handling here
-    } else {
-        const spotSearchResults = await ytsr(filterSpot.url, {pages : 1});
-        return {
-            "title": spotSearchResults.items[0].title,
-            "url": spotSearchResults.items[0].url
-        };
-    }
-}
-
 async function playlistLoader(url,queue,message){
     //build playlist
     let playlist = [];
     if(url.includes('spotify')){
         let playlistSp = await getTracks(url.substring(0,url.indexOf('?'))); //use url w/o parameters
         playlist = await Promise.all(playlistSp.map((track) =>{
-            return spotifyTrack(track.external_urls.spotify);
+            return getSpotifySong(track.external_urls.spotify);
         }));
     } else if (url.includes('youtube')){
         let playlistID = await ytpl.getPlaylistID(url);
@@ -231,30 +107,9 @@ async function playlistLoader(url,queue,message){
     }
 }
 
-async function drink(message,serverQueue){
-    console.log('user requested to pause');
-        serverQueue.connection.dispatcher.pause();
-        return message.channel.send(`Type ${prefix}leftovers to finish your meal!`);
-}
-
-async function leftovers(message,serverQueue){
-    console.log('user requested to resume');
-        serverQueue.connection.dispatcher.resume();
-        return message.channel.send(`Enjoy your leftovers`);
-}
-
-async function instantOats(message,serverQueue){
-    //code for a playnext function goes here
-}
-
 // export
 module.exports = {
-    execute,
     cook,
-    lunch,
-    cancel,
     menu,
-    playlistLoader,
-    drink,
-    leftovers
+    playlistLoader
 }
